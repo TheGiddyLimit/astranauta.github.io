4f5ec0b0ca50833e64938eb447c5d1a8a193ee70
Jeffrey Stocker <kagesennin@gmail.com>
2018-09-25 20:34:05 -0700

    added sorting carots to all search indexes

---
+++ js/sortCarret.js
@@ -0,0 +1,12 @@
+var $$filterButtonFunction = function () {
+	var $this = $(this);
+	var sorted = $this.hasClass("asc")
+
+	$("#filtertools").find(".caret").removeClass("caret");
+	$this.find("span").addClass("caret")
+		.toggleClass("caret-reverse", $this.hasClass("asc"));
+};
+
+$("#filtertools-mundane").find("button.sort").on(EVNT_CLICK, $$filterButtonFunction);
+$("#filtertools-magic").find("button.sort").on(EVNT_CLICK, $$filterButtonFunction);
+$("#filtertools").find("button.sort").on(EVNT_CLICK, $$filterButtonFunction);
